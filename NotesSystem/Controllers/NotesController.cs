75b8ab6591a5eaa20f85fb658bbfa125ec17b356
heyner palacios <heyner222@gmail.com>
2021-04-27 23:10:09 -0600

    added changes including datatable

--- NotesSystem/Controllers/NotesController.cs
+++ NotesSystem/Controllers/NotesController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NotesSystem.Data;
 using NotesSystem.Models;
+using NotesSystem.Models.ViewModels;
+using System.Linq.Dynamic.Core; 
 
 namespace NotesSystem.Controllers
 {
@@ -173,6 +175,84 @@ namespace NotesSystem.Controllers
             return PartialView("_StudentTable", student);
         }
 
+
+        public JsonResult GetTable(string name)
+        {
+            List<Student> students = _context.Student.Where(s => s.name.Contains(name)).ToList();
+            if (students.Count == 0)
+            {
+                students = _context.Student.Where(s => s.name.Equals(name)).ToList();
+
+            }
+
+            if (students.Count == 0)
+            {
+                ViewBag.errorMessage = "Student not found";
+            }
+
+            return Json(students);
+        }
+
+
+        public IActionResult ListStudent()
+        {
+            return View();
+        }
+
+        public string draw = "";
+        public string start = "";
+        public string length = "";
+        public string sortColumn = "";
+        public string sortColumnDir = "";
+        public string searchValue = "";
+        public int pageSize, skip, recordsTotal;
+
+        //by default every json request is a post
+        [HttpPost]
+        public IActionResult Json()
+        {
+            //here we extract the data sent by the framework datatable
+            var draw = Request.Form["draw"].FirstOrDefault();
+            var start = Request.Form["start"].FirstOrDefault();
+            var length = Request.Form["length"].FirstOrDefault();
+            var sortColumn = Request.Form["Columns["+Request.Form["order[0][column]"].FirstOrDefault()+"][name]"].FirstOrDefault();
+            var sortColumnDir = Request.Form["order[0][dir]"].FirstOrDefault();
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+            //we validate the data received 
+            pageSize = !length.Equals("") ? Convert.ToInt32(length) : 0;
+            skip = !start.Equals("") ? Convert.ToInt32(start) : 0;
+            recordsTotal = 0;
+            //we create a list to pass later the query
+            List<ViewModelStudent> student;
+
+            /* we create a query which will hold just the query to avoid pulling all date before pagination and search
+            for optimization porpouses it faster to get just 10 item than 100000 then we equal to the model view
+            to get only the values we want to pass to the view not all of the values from object we could also do 
+            orderby sortColumn+""+sortColumnDir to sort but it was done later by linq.dinamyc.core to validate it has an order with if */
+            IQueryable<ViewModelStudent> query = (from a in _context.Student where a.name.Contains(searchValue) select new ViewModelStudent{ 
+            name=a.name,
+            age=a.age
+            });
+
+            //we create the query by the search value here we are missing to validate all fields from the view
+            if (searchValue != "")
+            {
+                query = query.Where(a => a.name.Contains(searchValue));
+            }
+
+            //we check sortcolumn and sortcolumn direction
+            if(!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDir))
+            {
+                //to do order by it was necessary to add the reference or package from nugget System.Linq.Dynamic.Core
+                query = query.OrderBy(sortColumn + " " + sortColumnDir);
+            }
+            recordsTotal = query.Count();
+
+            student = query.Skip(skip).Take(pageSize).ToList();
+            return Json(new {draw=draw, recordsFiltered= recordsTotal, recordsTotal=recordsTotal, data=student});
+        }
+          
         // POST: Notes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
---
+++ NotesSystem/Models/ViewModels/ViewModelStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesSystem.Models.ViewModels
+{
+    public class ViewModelStudent
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
