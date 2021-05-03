8553b179e03359271260e90b0b99e5ae5626b6cc
heyner palacios <heyner222@gmail.com>
2021-05-02 23:04:48 -0600

    added file system

---
+++ NotesSystem/Controllers/FilesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesSystem.Controllers
+{
+    public class FilesController : Controller
+    {
+        private IHostingEnvironment _env;
+        private string dir;
+
+        public FilesController(IHostingEnvironment env)
+        {
+            _env = env;
+            dir = _env.ContentRootPath;
+        }
+
+        public IActionResult IndexFile() {
+           
+            
+            return View();
+        } 
+
+        public IActionResult UploadFile(IFormFile file)
+        
+        {
+          
+             
+            using(var fileStream=new FileStream(Path.Combine(dir+"/Files",file.FileName), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+            return RedirectToAction("IndexFile");
+        }
+
+        public IActionResult UploadFiles(ICollection<IFormFile>  files)
+
+        {
+
+            foreach(var item in files)
+            {
+                using (var fileStream = new FileStream(Path.Combine(dir + "/Files", item.FileName), FileMode.Create, FileAccess.Write))
+                {
+                    item.CopyTo(fileStream);
+                }
+            }
+           
+            return RedirectToAction("IndexFile");
+        }
+
+        public IActionResult FileInModel(FileForm file)
+        {
+
+            using (var fileStream = new FileStream
+                (Path.Combine(dir + "/Files", file.name), 
+                FileMode.Create, FileAccess.Write))
+            {
+              file.file.CopyTo(fileStream);
+            }
+            return RedirectToAction("IndexFile");
+        }
+    }
+}
---
+++ NotesSystem/Models/FileForm.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesSystem.Models
+{
+    public class FileForm
+    {
+
+        public string name { get; set; }
+
+        public IFormFile file { get; set; }
+    }
+}
